3d2ad575bd892e37976a54f6dd49b05541a7d7b5
Danel Lafuente <dlafuente003@gmail.com>
2024-05-22 19:23:52 +0200

    swagger y refactor de middleware

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/mongo.js";
 import router from "./routes/router.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json" assert { type: 'json' };;
 
 dotenv.config();
 const CONTAINER_PORT = 3000;
@@ -10,6 +12,9 @@ const app = express();
 app.use(express.json()); // api
 //app.use(express.urlencoded({extended:true})); // vistas
 connectDB();
+
+app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerDocument));
+
 app.get("/",(req,res)=>{
     res.json({message:"Hello World"});
 })
