466da6a77895e5a09e52b8729c206dc470eace83
danel-bbk <dlafuente003@gmail.com>
2024-05-23 15:33:25 +0200

    middleware arreglado

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -4,12 +4,11 @@ import userController from "../controllers/users/userController.js";
 const verifyToken = async (req, res, next) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
-    return res.status(401).json({ error: "no hay token jwt" });
+    return res.status(401).json({ error: "No hay token jwt" });
   }
 
+  const token = authorization.split(" ")[1];
   try {
-    const token = authorization.includes("Bearer")? authorization.split("Bearer ")[1] : authorization;
-    console.log("token", token);
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await userController.getById(decoded._id);
     if (!user) {
@@ -19,20 +18,24 @@ const verifyToken = async (req, res, next) => {
     next();
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: "ha habido un error" });
+    return res.status(400).json({ error: "Ha habido un error puede haber caducado el token" });
   }
 };
 
-const isAuthenticated = async (req, res, next) => {
-  await verifyToken(req, res, next);
+const isAuthenticated = (req, res, next) => {
+  verifyToken(req, res, next);
 };
 
-const isAdmin = async (req, res, next) => {
-  await verifyToken(req, res, next);
-  if (req.user.role !== "admin") {
-    return res.status(401).json({ error: "No estás autorizado" });
-  }
-  next();
+const isAdmin = (req, res, next) => {
+  verifyToken(req, res, (error) => {
+    if (error) {
+      return next(error);
+    }
+    if (req.user.role !== "admin") {
+      return res.status(401).json({ error: "No estás autorizado" });
+    }
+    next();
+  });
 };
 
 export { isAuthenticated, isAdmin };
