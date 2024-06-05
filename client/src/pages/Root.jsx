e18e662eb99ba538bca8f00767ad3ab741cc2e59
Danel Lafuente <dlafuente003@gmail.com>
2024-06-05 21:58:53 +0200

    favicon and title

--- client/src/pages/Root.jsx
+++ client/src/pages/Root.jsx
@@ -1,11 +1,11 @@
 import "../App.css";
-import { Outlet, Link,useNavigate } from "react-router-dom";
+import { Outlet, Link, useNavigate } from "react-router-dom";
 import { deleteToken, getToken } from "../utils/local";
-import { useEffect,useContext } from "react";
+import { useEffect, useContext } from "react";
 import UserContext from "../context/userContext";
 import { fetchUserData } from "../utils/fetch";
 const Root = () => {
-    const {user,setUser} = useContext(UserContext);
+    const { user, setUser } = useContext(UserContext);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -16,13 +16,13 @@ const Root = () => {
     }, []);
 
     async function getUserData() {
-        const data  = await fetchUserData();
-        if(data.error){
+        const data = await fetchUserData();
+        if (data.error) {
             navigate("/register");
         }
         setUser(data.data);
-      }
-    async function handleLogout(e){
+    }
+    async function handleLogout(e) {
         e.preventDefault();
         setUser(null);
         deleteToken();
@@ -33,7 +33,10 @@ const Root = () => {
             <nav>
                 <ul>
                     <li>
-                        <Link to="/">Home</Link>
+                        <Link to="/">
+                            <img className="logo" src="/qtrello.png" alt="logo" />
+                            Home
+                        </Link>
                     </li>
                     <li>
                         <Link to="/projects">Projects</Link>
@@ -43,6 +46,7 @@ const Root = () => {
                     </li>
                 </ul>
             </nav>
+
             <h1>Hello {user?.username}</h1>
             <Outlet />
         </div>
